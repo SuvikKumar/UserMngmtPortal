31e8e1b762cb124ea3dcd308434786933b753e12
SuvikKumar <89348815+SuvikKumar@users.noreply.github.com>
2024-04-25 16:58:29 +0530

    Delete Model user.js

--- Model user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  // ... existing user schema fields
-  role: { type: mongoose.Schema.Types.ObjectId, ref: "Role" }, // Reference role document
-  tenants: [{ type: mongoose.Schema.Types.ObjectId, ref: "Tenant" }], // User's associated tenants
-});
-
-module.exports = mongoose.model("User", userSchema);
